8a7975c64da4db75eea9efe478f268626a9ab0ff
soyuka <soyuka@gmail.com>
2015-02-26 08:35:09 +0100

    Remove treekill, don't kill detached child processes  #1036

--- test/bash/treekill.sh
+++ test/bash/treekill.sh
@@ -18,7 +18,7 @@ cleanup() {
 $pm2 kill
 
 echo "Starting and stoping a process which has child"
-echo "We're testing that childs are beeing killed even detached"
+echo "We're testing that childs are beeing killed if not detached"
 
 echo "Some logs" >> childrens.log
 echo "Some logs" >> childrensdetached.log
@@ -39,41 +39,14 @@ fi
 
 OUT=`ps -ef|grep 'tail -F ./childrensdetached.log'|grep -v grep|wc -l`
 
-if [ $OUT -eq 0 ]
+if [ $OUT -eq 1 ]
 then
-    success "We killed the child process"
+    success "We didn't killed the detached child process"
 else
     cleanup
-    fail "Failed to kill the child process"
+    fail "Failed not to kill the detached child process"
 fi
 
 $pm2 kill
-
-echo "Now let's enable --no-treekill"
-
-$pm2 start childrens.js --no-treekill
-
-$pm2 stop childrens.js
-
-OUT=`ps -ef|grep 'tail -F ./childrens.log'|grep -v grep|wc -l`
-
-ps -ef | grep 'tail -F'
-if [ $OUT -eq 0 ]
-then
-    success "Killed the child process!"
-else
-    cleanup
-    fail "Failed to kill the child process"
-fi
-
-OUT=`ps -ef|grep 'tail -F ./childrensdetached.log'|grep -v grep|wc -l`
-
-if [ $OUT -eq 1 ]
-then
-    success "We didn't killed the child process"
-else
-    cleanup
-    fail "Failed not to kill the child process"
-fi
 
 cleanup
